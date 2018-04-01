eaec4ff2e079d3bf8a9ea139aaf548a3f3a5cc3f
Paul G Webster <paul.g.webster@googlemail.com>
2018-04-01 08:20:15 +0100

    Starting point

---
+++ AltThread/AltThread/AltThread.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace AltThread
+{
+    public class ThreadController
+    {
+        public ThreadController(
+            ThreadControllerConfig Config
+        )
+        {
+
+        }
+
+
+
+    }
+
+    public class ThreadControllerConfig
+    {
+        public string Name { get; internal set; }
+    }
+
+    public class UniversalDataBus
+    {
+
+    }
+}
---
+++ AltThread/AltThread/Main.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AltThread;
+
+namespace Example
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            ThreadController x = new ThreadController(
+                new ThreadControllerConfig() { Name="test" }
+            );
+        }
+    }
+}
